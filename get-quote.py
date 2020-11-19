ef91502db3d2f0bddca047496776c6f33955a8af
mankal111 <mankal111@hotmail.com>
2020-11-19 20:37:39 +0200

    Renamed the primary function

--- get-quote.py
+++ get-quote.py
@@ -1,4 +1,4 @@
-def main():
+def kyria():
   print("Keep it logically awesome.")
 
   #f = open("quotes.txt")
@@ -8,4 +8,4 @@ def main():
   #print(quotes)
 
 if __name__== "__main__":
-  main()
+  kyria()
